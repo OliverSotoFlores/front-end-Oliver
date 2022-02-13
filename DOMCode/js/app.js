43a291df866e1b107e4e651d9f2efc9c55300348
Soto <oliver-shaddai.soto@atos.net>
2022-02-12 18:48:13 -0600

    Leasson 15: Search bar

--- DOMCode/js/app.js
+++ DOMCode/js/app.js
@@ -154,3 +154,18 @@ hideBox.addEventListener('change',function (e) {
     list.style.display = 'initial'
   }
 })
+
+//leasson 15 : search
+const searchbar = document.forms['search-books'].querySelector('input');
+searchbar.addEventListener('keyup',function(e) {
+  const term = e.target.value.toLowerCase();
+  const books = list.getElementsByTagName('li');
+  Array.from(books).forEach(function(book) {
+      const title = book.firstElementChild.textContent;
+      if (title.toLowerCase().indexOf(term) != -1) {
+        book.style.display='block';
+      }else {
+        book.style.display = 'none';
+      }
+  })
+})
