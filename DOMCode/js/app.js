4c766db8fc04f207f97890f11526105ecc7649a7
Soto <oliver-shaddai.soto@atos.net>
2022-02-12 18:58:58 -0600

    Leasson 16: Tabbed content

--- DOMCode/js/app.js
+++ DOMCode/js/app.js
@@ -169,3 +169,20 @@ searchbar.addEventListener('keyup',function(e) {
       }
   })
 })
+
+
+//Leasson 16: Tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', function(e) {
+  if (e.target.tagName == 'LI') {
+    const targetPanel = document.querySelector(e.target.dataset.target);
+    panels.forEach(function(panel) {
+      if (panel == targetPanel) {
+        panel.classList.add('active')
+      }else {
+        panel.classList.remove('active')
+      }
+    })
+  }
+})
