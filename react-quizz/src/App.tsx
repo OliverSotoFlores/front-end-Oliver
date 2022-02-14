b0b35f46d6becb785c11a6b7f5c6a47d61aa5689
Soto <oliver-shaddai.soto@atos.net>
2022-02-14 15:30:47 -0600

    Get the questions

--- react-quizz/src/App.tsx
+++ react-quizz/src/App.tsx
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { useState}  from 'react';
+import { fetchQuizQuestions } from './components/API';
+import QuestionCard from './components/QuestionCard';
 
-function App() {
-  return (
+//Types
+import { QuestionState, Difficulty } from './components/API';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
+
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestion] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+  
+
+  
+  
+  const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestion(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  };
+
+const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+};
+
+const nextQuestion = () => {
+
+
+};
+
+return (
     <div className="App">
-     quizz
+      <h1>REAT QUIZZ</h1>
+      <button className='start' onClick={startTrivia}>
+        Start
+      </button>
+      <p className='score'>Score:</p>
+      <p className=''>Loading question...</p>
+      {/* <QuestionCard 
+        questionNumber={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      /> */}
+      <button className='next' onClick={nextQuestion}>Next question</button>
+
     </div>
   );
 }
